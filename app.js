90aea55f8e58415b3567090da1c99f8535f533f5
morfeii <morfeii88@gmail.com>
2020-04-15 23:24:41 +0300

    implement post, delete, put methods

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const path = require('path');
+const { v4 } = require('uuid');
 const app = express();
 
-const CONTACTS = [
-  { id: 1, name: 'AAA', value: '12324325', marked: false },
+let CONTACTS = [
+  { id: v4(), name: 'AAA', value: '12324325', marked: false },
 ];
 
+app.use(express.json());
+
+// GET
 app.get('/api/contacts', (req, res) => {
   setTimeout(() => {
     res.status(200).json(CONTACTS)
   }, 1000)
-})
+});
+
+// POST
+app.post('/api/contacts', (req, res) => {
+  const contact = { ...req.body, id: v4(), marked: false };
+  CONTACTS.push(contact);
+  res.status(201).json(contact);
+});
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+  CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+  res.status(200).json('Contact was deleted');
+});
+
+// PUT
+app.put('/api/contacts/:id', (req, res) => {
+  const idx = CONTACTS.findIndex(c => c.id === req.params.id);
+  CONTACTS[idx] = req.body;
+  res.json(CONTACTS[idx]);
+});
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 
--- client/frontend.js
+++ client/frontend.js
@@ -28,18 +28,25 @@ new Vue({
     }
   },
   methods: {
-    createContact() {
+    async createContact() {
       const {...contact} = this.form;
 
-      this.contacts.push({ ...contact, id: Date.now(), marked: false });
+      const newContact = await request('/api/contacts', 'POST', contact);
+
+      this.contacts.push(newContact);
       this.form.name = '';
       this.form.value = '';
     },
-    markContact(id) {
+    async markContact(id) {
       const contact = this.contacts.find(c => c.id === id);
-      contact.marked = true;
+      const updated = await request(`/api/contacts/${id}`, 'PUT', {
+        ...contact,
+        marked: true,
+      });
+      contact.marked = updated.marked;
     },
-    deleteContact(id) {
+    async deleteContact(id) {
+      await request(`/api/contacts/${id}`, 'DELETE');
       this.contacts = this.contacts.filter(c => c.id !== id);
     },
   },
@@ -62,7 +69,8 @@ async function request(url, method = 'GET', data = null) {
 
     const response = await fetch(url, {
       method,
-      headers
+      headers,
+      body,
     });
     return await response.json();
   } catch (error) {
